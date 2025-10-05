91667a4db6d4a8851dfa90a6ba3df39813bf5a78
Stronhina Kateryna <liksmirn8@gmail.com>
2025-10-05 23:48:28 +0300

    feat: добавил dispatch and action, action creator, action type а также поле сообщений и кнопка addMessage

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,11 @@
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem.jsx';
 import Message from './Message/Message.jsx';
+import React from 'react';
+import {addMessageActionCreator, onMessageChangeActionCreator} from './../redux/state.js';
 
 const Dialogs = (props) => {
-  const { dialogs, messages } = props.dialogsPage;
+  const { dialogs, messages, newMessageText } = props.dialogsPage;
 
   let dialogsElements = dialogs.map((dialog) => (
     <DialogItem key={dialog.id} name={dialog.name} id={dialog.id} src={dialog.src} />
@@ -13,10 +15,25 @@ const Dialogs = (props) => {
     <Message key={message.id} message={message.message} />
   ));
 
+  // let newMessageElement = React.createRef();
+
+   const onMessageChange = (e) => {
+    let text = e.current.value;
+    props.dispatch(onMessageChangeActionCreator(text));
+  };
+   const addMessage = () => {
+    props.dispatch(addMessageActionCreator());
+  };
   return (
     <div className={s.dialogs}>
       <div>{dialogsElements}</div>
       <div>{messagesElements}</div>
+      <div>
+        <textarea onChange={onMessageChange}  value={newMessageText}/>
+      </div>
+      <div>
+        <button onClick={addMessage}>Add message</button>
+      </div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post.jsx';
+import {addPostActionCreator} from './../../redux/state.js';
+import {updateNewPostTextActionCreator} from './../../redux/state.js';
+
 
 const MyPosts = (props) => {
   const { posts, newPostText } = props.profilePage;
   let postsElements = posts.map((post) => (
     <Post key={post.id} message={post.message} likesCount={post.likesCount} />
   ));
-  const addPost = () => {
-    // let text = newPostElement.current?.value ?? '';
 
-    props.addPost();
-  };
   let newPostElement = React.createRef();
+
+  const addPost = () => {
+    // props.addPost();
+    props.dispatch(addPostActionCreator());
+  };
+
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.updateNewPostText(text);
+    // props.updateNewPostText(text);
+    let action = updateNewPostTextActionCreator(text);
+    props.dispatch(action);
   };
   return (
     <div className={s.postBlock}>
@@ -28,9 +35,7 @@ const MyPosts = (props) => {
         <div>
           <button onClick={addPost}>Add post</button>
         </div>
-        <div>
-          <button>Remove</button>
-        </div>
+
       </div>
       <div className={s.posts}>{postsElements}</div>
     </div>
